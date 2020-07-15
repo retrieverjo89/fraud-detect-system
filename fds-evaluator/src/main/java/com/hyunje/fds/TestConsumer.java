424f0113a4abd84b5fb80bac22fa317f7d12a73b
Hyunje Jo <retriever89@gmail.com>
2020-07-16 01:51:31 +0900

    kafka 테스트 위한 producer, consumer 작성

---
+++ fds-evaluator/src/main/java/com/hyunje/fds/TestConsumer.java
@@ -0,0 +1,61 @@
+package com.hyunje.fds;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+public class TestConsumer {
+    private final static String TOPIC = "fds.test";
+    private final static String BOOTSTRAP_SERVER = "localhost:9092";
+    private final static String PARTITION_KEY = "0";
+
+    private static Consumer<Long, String> createConsumer() {
+        Properties props = new Properties();
+
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "EXAMPLE_CONSUMER");
+
+        Consumer<Long, String> consumer = new KafkaConsumer<Long, String>(props);
+        consumer.subscribe(Collections.singletonList(TOPIC));
+
+        return consumer;
+    }
+
+    static void runConsumer() throws InterruptedException {
+        final Consumer<Long, String> consumer = createConsumer();
+
+        final int giveUp = 100;   int noRecordsCount = 0;
+
+        while (true) {
+            final ConsumerRecords<Long, String> consumerRecords =
+                    consumer.poll(1000);
+
+            if (consumerRecords.count()==0) {
+                noRecordsCount++;
+                if (noRecordsCount > giveUp) break;
+                else continue;
+            }
+
+            consumerRecords.forEach(record -> {
+                System.out.printf("Consumer Record:(%d, %s, %d, %d)\n",
+                        record.key(), record.value(),
+                        record.partition(), record.offset());
+            });
+
+            consumer.commitAsync();
+        }
+        consumer.close();
+        System.out.println("DONE");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        runConsumer();
+    }
+}
